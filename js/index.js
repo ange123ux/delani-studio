116e5386e8882268d850606df646e47f8ee4a54e
Ange Uwimana <uuwange@gmail.com>
2020-10-12 08:31:13 +0200

    Added the class of contact to be responsive

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+    $(".design").click(function () {
+        $("#one").toggle();
+    });
+});
+$(".icon").click(function () {
+    $("#two").toggle();
+});
+         
+$(".product").click(function () {
+    $("#three").toggle();
+});
+
+$(document).ready(function(){
+    alert("Hello! Thank you for reaching out to us.")
+});
+    
